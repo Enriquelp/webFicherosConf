2f0bbaf94b623147be64907493d3f1e1a372439b
Enriquelp <enriquelopezencinas@uma.es>
2024-09-05 16:07:26 +0200

    Añadidos modelos uvl, paginas para mostrar los modelos y correcciones menores

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -9,6 +9,12 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/load_page/<page>')
+def load_page(page):
+    if page not in ['docker', 'kubernetes']:
+        abort(404)  # Si la página no es válida, devolver un error 404
+    return render_template(f'{page}.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
