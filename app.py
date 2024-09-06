bfde0caae6369bf729b518354d4ffb3082db1bbd
Enriquelp <enriquelopezencinas@uma.es>
2024-09-06 15:57:36 +0200

    Los fm ahora se muestran en a web, elimandas las paginas de Docker y Kubernetes, añadido requeriments y modificados los modelos uvl

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, make_response
+from flamapy.metamodels.fm_metamodel.transformations import UVLReader
+from flamapy.metamodels.fm_metamodel.models import FeatureModel, Constraint
+from flamapy.metamodels.fm_metamodel.transformations import JSONWriter, JSONReader
+from typing import Optional
+import os
 
 app = Flask(__name__)
+
+pathFM = 'static/feature_models/'
 
 @app.route('/')
 def index():
@@ -13,12 +20,25 @@ def about():
 @app.route('/load_page/<page>')
 def load_page(page):
     if page not in ['docker', 'kubernetes']:
-        abort(404)  # Si la página no es válida, devolver un error 404
+        abort(404)
     return render_template(f'{page}.html')
+
+@app.route('/load_fm/<model>')
+def load_model(model):
+    fm = f'{pathFM}{model}.uvl'
+    if not os.path.exists(fm):
+        abort(404)
+    return read_fm_file(fm)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+def read_fm_file(filename: str) -> Optional[FeatureModel]:
+    fm = UVLReader(filename).transform()
+    json_fm = JSONWriter(path=None, source_model=fm).transform()
+    response = make_response(json_fm)
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
