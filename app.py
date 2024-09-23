d64d7c1f111af0d5bc813282292e966a6158a6dc
Enriquelp <enriquelopezencinas@uma.es>
2024-09-23 14:15:20 +0200

    Añadido descarga de archivos de configuracion

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import sys
-from flask import Flask, flash, render_template, abort, make_response, request, send_from_directory
+sys.path.append('./static/script')  # Agrega el directorio al path para importar el módulo main_resolve_variability
+from flask import Flask, flash, jsonify, render_template, abort, make_response, request, send_file, send_from_directory
 from flamapy.metamodels.fm_metamodel.transformations import UVLReader
 from flamapy.metamodels.fm_metamodel.models import FeatureModel, Constraint
 from flamapy.metamodels.fm_metamodel.transformations import JSONWriter, JSONReader
 from typing import Optional
 import os
 import subprocess
+from static.script.main_resolve_variability import main as generate_conf
 
 app = Flask(__name__)
 
 pathFM = 'static/feature_models/' # Ruta hacia los FM
-app.config['UPLOAD_FOLDER'] = 'uploads/' # Configurar el folder donde se guardarán los archivos subidos
+app.config['UPLOAD_FOLDER'] = 'uploads' # Configurar el folder donde se guardarán los archivos subidos
 app.secret_key = 'supersecretkey'  # Para habilitar mensajes flash
 
 @app.route('/')
@@ -47,56 +49,44 @@ def upload_file():
     if file and file.filename:
         # Guardar el archivo temporalmente
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        dest = os.path.join(app.config['UPLOAD_FOLDER'], 'config')
         file.save(filepath)
-        print(f'filepath: {filepath}')
 
+        # Eliminar el archivo de configuración si existe antes de generar el nuevo
+        if os.path.exists(dest):
+            os.remove(dest)
+
+        # Procesar el archivo subido y crear el archivo de config.yaml
         try:
-            # Procesar el archivo subido
             if feature_model == 'docker':
                 template = 'static/templates/dockerfile_conf_template.txt.jinja'
                 mapping = 'static/mapping_models/dockerfile_conf_mapping.csv'
-                configuration = generate_conf(filepath, mapping, template, False, False, True)
+
+                # Procesar el archivo subido y crear el archivo de config.txt
+                configuration = generate_conf(filepath, mapping, template, False, False, True, dest)
+
+                # Eliminar el archivo JSON después de procesarlo
+                os.remove(filepath)
+                return send_file(dest, as_attachment=True, download_name='Dockerfile')
+            
             elif feature_model == 'kubernetes': 
                 template = 'static/templates/Kubernetes_manifest_v2/Kubernetes_manifest_base.txt.jinja'
                 mapping = 'static/mapping_models/Kubernetes_manifest_mapping_v2.csv'
-                configuration = generate_conf(filepath, mapping, template, True, False, False)
-                # Guardar el archivo de configuración
-                with open("uploads/config.yaml", "w") as f: # Guarda el resultado en un fichero 
-                    print('\n')
-                    print_without_blank_lines(configuration, f)
-                    print('\n')
-            
-            # Si el archivo se procesó correctamente, devolver el archivo de configuración
-            send_from_directory(app.config['UPLOAD_FOLDER'], 'config.yaml', as_attachment=True)
 
-            # Eliminar el archivo después de procesarlo
-            os.remove(filepath)
-            flash('Archivo procesado y eliminado correctamente')
-        
+                # Procesar el archivo subido y crear el archivo de config.yaml
+                configuration = generate_conf(filepath, mapping, template, True, False, False, dest)
+                
+                # Eliminar el archivo JSON después de procesarlo
+                os.remove(filepath)
+                return send_file(dest, as_attachment=True, download_name='config.yaml')
+            
+        # Manejo de errores si el procesamiento falla
         except Exception as e:
-            # Manejo de errores si el procesamiento falla
-            flash(f'Error al procesar el archivo: {str(e)}')
-            # Asegurarse de eliminar el archivo si ocurre un error
+            # Asegurarse de eliminar el archivo JSON si ocurre un error
             if os.path.exists(filepath):
                 os.remove(filepath)
-        
-        
-    return render_template('index.html', correct='ok')
-
-# Función para generar la configuración final a partir del JSON subido
-def generate_conf(config, map, template, kubernetes, details, dockerfile):
-    # Llamar al script utilizando subprocess
-    try:
-        conf = subprocess.run(
-            ['python', 'static/script/main_resolve_variability.py', config, map, template, kubernetes, details, dockerfile],
-            capture_output=True, text=True
-        )
-
-        # Devolver el resultado del script
-        return conf 
+            return jsonify({"error": f"Error al procesar el archivo: {str(e)}"}), 500
     
-    except Exception as e:
-        return str(e) 
 
 # Manejo de errores 404
 @app.errorhandler(404)
@@ -108,13 +98,6 @@ def read_fm_file(filename: str) -> Optional[FeatureModel]:
     json_fm = JSONWriter(path=None, source_model=fm).transform()
     response = make_response(json_fm)
     return response
-
-def print_without_blank_lines(text, file):
-    for line in text.splitlines():
-        if line.strip():  # Verifica si la línea no está en blanco
-            sys.stdout.write(line + '\n')  # Escribe la línea en stdout
-            file.write(line + '\n')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script/main_resolve_variability.py
+++ static/script/main_resolve_variability.py
@@ -10,21 +10,10 @@ from spl_implementation.utils import utils
 from spl_implementation.models import VEngine
 import subprocess
 
-import click
-
-# ARGUMENTOS CLICK
-@click.command()
-@click.option('--config', '-c', required=True, type=click.Path(exists=True), default='', help='Ruta al fichero de configuracion del FM')
-@click.option('--map', '-m', required=True, type=click.Path(exists=True), default='', help='Ruta al fichero del mapping')
-@click.option('--template', '-t', required=True, type=click.Path(exists=True), default='', help='ruta al fichero template')
-@click.option('--kubernetes', '-k', is_flag=True, help='Indica si se debe comprobar la validez del resultado para kubernetes')
-@click.option('--details', is_flag=True, help='Indica si se debe dar detalles de optimización del fichero YAML')
-@click.option('--dockerfile', '-d', is_flag=True, help='Indica si se debe comprobar la validez del resultado para kubernetes')
-
-def main(config, map, template, kubernetes, details, dockerfile):
-    kubeconform = "validators/kubeconform.exe"
-    kube_score = "validators/kube-score.exe"
-    hadolint = 'validators/hadolint.exe'
+def main(config, map, template, kubernetes, details, dockerfile, dest):
+    kubeconform = "static/validators/kubeconform.exe"
+    kube_score = "static/validators/kube-score.exe"
+    hadolint = 'static/validators/hadolint.exe'
     vengine = VEngine()
 
     vengine.load_configuration(config)
@@ -33,28 +22,15 @@ def main(config, map, template, kubernetes, details, dockerfile):
 
     result = vengine.resolve_variability()
 
-    with open("uploads/config.yaml", "w") as f: # Guarda el resultado en un fichero 
-        print('\n')
+    with open(dest, "w") as f: # Guarda el resultado en un fichero 
         print_without_blank_lines(result, f)
-        print('\n')
-    
-    if(kubernetes): # Comprueba si existe algun error en el YAML de kubernetes
-        rdo = subprocess.run([kubeconform, "salida.yaml"], capture_output=True, text=True)
-        if (rdo.stdout != ''):
-            print(f"\033[91m ERROR: {rdo.stdout} \033[0m") # Imprime el resultado de la comprobacion en rojo
-        if(details):
-            details = subprocess.run([kube_score, "score", "salida.yaml"], capture_output=True, text=True) # Analiza el resultado en busca de recomendaciones
-            print(f"\033[92m RECOMMENDATIONS: {details.stdout} \033[0m")
-    if (dockerfile):
-         rdo = subprocess.run([hadolint, "salida.yaml"], capture_output=True, text=True)
-         print(f"\033[91m ERROR: {rdo.stdout} \033[0m") # Imprime el resultado de la comprobacion en rojo
-    return result, rdo.stdout
 
+    return result
 
 def print_without_blank_lines(text, file):
     for line in text.splitlines():
         if line.strip():  # Verifica si la línea no está en blanco
-            sys.stdout.write(line + '\n')  # Escribe la línea en stdout
+            # sys.stdout.write(line + '\n')  # Para mostrar la configuracion en la terminal
             file.write(line + '\n')
 
 if __name__ == '__main__':
